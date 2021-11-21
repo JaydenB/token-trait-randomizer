b5f4c4bb054f9aaf863d7d2fa861b3b3ad54f44f
JaydenB <jayden@westnet.com.au>
2021-11-21 13:25:49 +1100

    removed external 'generated' folder dependency

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ SEED = 0
 if __name__ == '__main__':
     traits.run(
         input_filepath=FILE_PATH,
-        output_filepath="generated/",
+        output_filepath="",
         count=TOKEN_COUNT,
         seed=SEED,
         print_rarities=10,
-        dry_run=True
+        dry_run=False
     )
