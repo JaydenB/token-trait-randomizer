be4bc7604b000622c6716b253ab61e5d14c63763
JaydenB <jayden@westnet.com.au>
2021-11-21 13:16:47 +1100

    updated default run

--- run.py
+++ run.py
@@ -6,12 +6,10 @@ TOKEN_COUNT = 1111
 SEED = 0
 
 if __name__ == '__main__':
-    generator = traits.TraitGenerator()
-    generator.load_traits(filepath=FILE_PATH)
-
-    generator.generate(count=TOKEN_COUNT, seed=SEED)
-    generator.save_to_file()
-    print("")
-
-    print(generator.top_rarity(count=10))
-    print(generator.bottom_rarity(count=10))
+    traits.run(
+        input_filepath=FILE_PATH,
+        output_filepath="generated/",
+        count=TOKEN_COUNT,
+        seed=SEED,
+        dry_run=True
+    )
